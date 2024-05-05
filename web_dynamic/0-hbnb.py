49e98355449f52aca724e24696dab3aad356ae67
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-05-05 16:55:50 +0300

    chore: Remove unused code and update Flask app route

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,39 +1,26 @@
 #!/usr/bin/python3
-""" Starts a Flask Web Application """
-import uuid
-
-from flask import Flask, render_template
-
+""" Starts a Flash Web Application """
 from models import storage
+from models.state import State
+from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from models.state import State
-
+from os import environ
+from flask import Flask, render_template
 app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
-def close_db(_):
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
     storage.close()
 
 
 @app.route('/0-hbnb', strict_slashes=False)
 def hbnb():
-    """
-    Renders the '0-hbnb.html' template with the necessary data.
-
-    Retrieves all states, amenities, and places from the storage.
-    Sorts the states, amenities, and places alphabetically.
-    Creates a list of states and their corresponding cities.
-    Generates a cache_id using the uuid module.
-
-    Returns:
-        The rendered template '0-hbnb.html' with the following parameters:
-        - states: A list of states and their corresponding cities.
-        - amenities: A list of amenities.
-        - places: A list of places.
-        - cache_id: A unique identifier for caching purposes.
-    """
+    """ HBNB is alive! """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
@@ -47,12 +34,12 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
+    return render_template('100-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places,
-                           cache_id=uuid.uuid4())
+                           places=places)
 
 
 if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
